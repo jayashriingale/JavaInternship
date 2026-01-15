2290e5c991e2c90b91914ea5f6d3250712629f13
jayashriingale <jayashridingale21@gmail.com>
2026-01-15 21:16:24 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+// This is a simple Java program
+public class HelloWorld {
+
+    // main method: program execution starts here
+    public static void main(String[] args) {
+        System.out.println("Hello, Java World!");
+    }
+}
